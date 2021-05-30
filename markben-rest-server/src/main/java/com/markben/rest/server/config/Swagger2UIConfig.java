a7c3458f9241ef2212a5603589b2db977178b058
乌草坡 <twxjlmq@aliyun.com>
2021-05-31 00:56:22 +0800

    新增测试服务模块

---
+++ markben-rest-server/src/main/java/com/markben/rest/server/config/Swagger2UIConfig.java
@@ -0,0 +1,66 @@
+package com.markben.rest.server.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * 配置Swagger2插件
+ * @author 乌草坡
+ * @since 1.0
+ */
+@Configuration
+@EnableOpenApi
+public class Swagger2UIConfig {
+
+    /**
+     * 是否开启swagger配置，生产环境需关闭
+     */
+    @Value("${swagger.enabled}")
+    private boolean enable;
+
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).enable(enable)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 创建该API的基本信息（这些基本信息会展现在文档页面中）
+     * 访问地址：http://项目实际地址/swagger-ui.html
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Markben框架REST APIs")
+                .description(getDescription())
+                .termsOfServiceUrl("")
+                .version("1.0")
+                .build();
+    }
+    
+    private String getDescription() {
+        String description = "注：在获取data或result属性内容时，必须要确保status值为1，否则无法获取到数据";
+        return description;
+    }
+
+}
